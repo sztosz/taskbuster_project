e64af22b810065d92887e208b578e7a71d7fb026
Bartosz Nowak <sztosz@gmail.com>
2015-06-04 08:05:36 +0200

    Database setup

--- taskbuster/settings/base.py
+++ taskbuster/settings/base.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'taskbuster.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'taskbuster_db',
+        'USER': 'tb',
+        'PASSWORD': 'tbpass',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
